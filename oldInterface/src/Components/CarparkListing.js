7c3f44abaa36ff8afa576e3ca8ba54c8fbc53611
Ng Ee Hooi <eng85@uwo.ca>
2020-11-04 17:12:33 +0800

    Tweak data to reduce correct selection

--- oldInterface/src/Components/CarparkListing.js
+++ oldInterface/src/Components/CarparkListing.js
@@ -24,7 +24,10 @@ class CarparkListing extends Component {
           :
           <div className="d-flex justify-content-between carpark-listing even p-1">
               <div className="w-75">
-                <div className="id">{this.props.id}</div>
+                <div>
+                  <span className="id">{this.props.id}</span>
+                  <span className="lot-type">{this.props.lotType!=="Visitor"? " (" + this.props.lotType + ")" : " "}</span>
+                </div>
                 <div className="location">{this.props.location}</div>
                 <div className="distance">Distance: {this.props.distance}m</div>
               </div>
